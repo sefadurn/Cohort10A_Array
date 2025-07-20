55a02180bc7ca5b0fd32264f2d596d737284a82b
user <user@DESKTOP-U6UHBUC>
2025-07-20 12:10:34 +0300

    Proje dosyası ekle.

---
+++ Array/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ Bu pratikte aşağıdaki 4 aşamayı kodlamanız isteniyor.
+
+1 - 10 adet tam sayı verisi alacak bir dizi tanımlayınız.
+
+2 - Bu dizinin elemanlarını bir for döngüsü ile doldurup, foreach döngüsü ile tekrar ekrana yazınız.
+
+3 - Bu diziye kullanıcıdan alınan yeni bir değeri ekleyiniz (11. eleman olarak)
+
+4 - Bu diziyi büyükten küçüğe ekrana yazdırınız.
+
+*/
+
+int[] dizi1 = new int[10];  // 10 adet tam sayi tutacak bir dizi tanımladık.
+
+for (int i = 0; i < dizi1.Length; i++)  // For döngüsü ile diziyi otomatik olarak doldurduk.
+{
+    dizi1[i] = i + 1;  // 1'den 10'a kadar sayilar atanıyor.
+
+}
+
+foreach(int i in dizi1)
+{
+    Console.WriteLine(i);
+}
+
+Console.Write("11.sayiyi giriniz: ");
+int yeniSayi = Convert.ToInt32(Console.ReadLine());
+
+int[] dizi2 = new int[11];  // Yeni, 11 elemanli bir dizi tanımladık.
+
+for(int i =0;i <dizi2.Length;i++)
+{
+    dizi2[i] = dizi1[i];
+
+}
+dizi2[10]= yeniSayi;   // Kullanıcının girmiş olduğu sayi 11.eleman olarak atanıyor.
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿// 1 - 10 adet tam sayı verisi alacak bir dizi tanımlanıyor
+int[] sayilar = new int[10];
+
+// 2 - for döngüsü ile diziyi kullanıcıdan veri alarak doldurma
+for (int i = 0; i < sayilar.Length; i++)
+{
+    Console.Write($"{i + 1}. sayıyı girin: ");
+    sayilar[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// 2 - foreach döngüsü ile diziyi ekrana yazma
+Console.WriteLine("\nGirdiğiniz 10 sayı:");
+foreach (int sayi in sayilar)
+{
+    Console.WriteLine(sayi);
+}
+
+// 3 - Yeni bir sayı alınıyor ve 11 elemanlı yeni diziye aktarılıyor
+Console.Write("\n11. sayıyı girin: ");
+int yeniSayi = Convert.ToInt32(Console.ReadLine());
+
+// 11 elemanlı yeni dizi oluşturuluyor
+int[] yeniDizi = new int[11];
+
+// Eski dizinin elemanları yeni diziye kopyalanıyor
+for (int i = 0; i < sayilar.Length; i++)
+{
+    yeniDizi[i] = sayilar[i];
+}
+
+// 11. eleman ekleniyor
+yeniDizi[10] = yeniSayi;
+
+// 4 - Dizi büyükten küçüğe sıralanıyor:
+// önce küçükten büyüğe sıralanır
+Array.Sort(yeniDizi);
+
+// sonra ters çevrilerek büyükten küçüğe hale getirilir
+Array.Reverse(yeniDizi);
+
+// 4 - Sıralanmış diziyi ekrana yazdır
+Console.WriteLine("\nBüyükten küçüğe sıralı dizi:");
+foreach (int sayi in yeniDizi)
+{
+    Console.WriteLine(sayi);
+}
